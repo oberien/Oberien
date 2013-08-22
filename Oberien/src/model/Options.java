39729c21460abe818649ba18f12db0751319aeb5
Blackhole16 <jaro.fietz@gmx.de>
2013-08-22 21:50:51 +0200

    IO of Options Improved FPS by calculating sight only after moving

--- Oberien/src/model/Options.java
+++ Oberien/src/model/Options.java
@@ -1,5 +1,12 @@
 package model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Options {
 	/**
 	 * indicates wheather the map is buffered (faster, high RAM) or not (slower, low RAM)
@@ -27,5 +34,40 @@ public class Options {
 	 * sets the loading speed (0-1)<br>
 	 * higher values result in less notifications but in a higher load speed
 	 */
-	public static float loadingSpeed = 0.001f;
+	public static double loadingSpeed = 0.001;
+	
+	public static void save() {
+		try {
+			File f = new File("cfg/settings.properties");
+			if (!f.exists()) {
+				f.mkdirs();
+				f.createNewFile();
+			}
+			Properties properties = new Properties();
+			properties.setProperty("bufferMap", bufferMap + "");
+			properties.setProperty("vsync", vsync + "");
+			properties.setProperty("screenMode", screenMode + "");
+			properties.setProperty("onlyUpdateWhenVisible", onlyUpdateWhenVisible + "");
+			properties.setProperty("antiAliasing", antiAliasing + "");
+			properties.setProperty("loadingSpeed", loadingSpeed + "");
+			properties.store(new FileOutputStream("cfg/settings.properties"), null);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void load() {
+		try {
+			Properties properties = new Properties();
+			properties.load(new FileInputStream("cfg/bounds.properties"));
+			bufferMap = Boolean.parseBoolean(properties.getProperty("bufferMap"));
+			vsync = Boolean.parseBoolean(properties.getProperty("vsync"));
+			screenMode = Integer.parseInt(properties.getProperty("screenMode"));
+			onlyUpdateWhenVisible = Boolean.parseBoolean(properties.getProperty("onlyUpdateWhenVisible"));
+			antiAliasing = Boolean.parseBoolean(properties.getProperty("antiAliaising"));
+			loadingSpeed = Double.parseDouble(properties.getProperty("loadingSpeed"));
+		} catch (FileNotFoundException e) {
+			save();
+		} catch (IOException e) {e.printStackTrace();}
+	}
 }
