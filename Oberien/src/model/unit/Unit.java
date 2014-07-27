5375231f42d585afab527ddd528eb777968f29c4
HigHt3k <johanntoepfer@gmail.com>
2014-07-27 14:42:25 +0200

    readded 'canPass' and 'canPass(byte b)' in Unit

--- Oberien/src/model/unit/Unit.java
+++ Oberien/src/model/unit/Unit.java
@@ -4,8 +4,10 @@ import model.Layer;
 import model.Model;
 import model.Player;
 import model.Type;
+import model.map.FieldList;
 
 public class Unit extends Model {
+	private byte[] canPass;
 	
 	public Unit(String name, Type type, int id, Player player,
 			int costMoney, int costEnergy, int costPopulation, 
@@ -15,6 +17,16 @@ public class Unit extends Model {
 				costMoney, costEnergy, costPopulation, 
 				maxLife, defense, viewrange, 
 				movespeed, moveGround, moveWater, moveAir, moveUnderground, moveUnderwater, defaultLayer);
+		this.canPass = FieldList.getInstance().getCanPass(type);
+	}
+	
+	public boolean canPass(byte b) {
+		for (int i = 0; i < canPass.length; i++) {
+			if (canPass[i] == b) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
 
