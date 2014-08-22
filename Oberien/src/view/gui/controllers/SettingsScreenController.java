9b1a29e984df4ee0443f1a3cf535061ee275984c
Bobthepeanut <bensteffan@gmx.de>
2014-08-22 22:40:00 +0200

    Corrected the main.xml. Added SettingsScreenController,

---
+++ Oberien/src/view/gui/controllers/SettingsScreenController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package view.gui.controllers;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+public class SettingsScreenController implements ScreenController {
+
+	@Override
+	public void bind(Nifty nifty, Screen screen) {
+	}
+
+	@Override
+	public void onStartScreen() {
+	}
+
+	@Override
+	public void onEndScreen() {
+	}
+
+}
